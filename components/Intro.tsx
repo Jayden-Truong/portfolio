0dea7caff1d353b340fc58a7593375aae93529de
Minh Huy <tmh.pug@gmail.com>
2023-10-24 12:04:58 +0700

    Create Intro Portfolio

---
+++ components/Intro.tsx
@@ -0,0 +1,81 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { BsArrowRight, BsLinkedin, BsGithub } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ type: 'tween', duration: 0.2 }}
+        >
+          <Image
+            src="https://images.unsplash.com/photo-1545310834-cd6a8a0884b8?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Avatar"
+            width={192}
+            height={192}
+            quality={95}
+            priority={true}
+            className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+          />
+        </motion.div>
+      </div>
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      >
+        <span className="font-bold">Hello, I'm Jayden.</span> I'm a{' '}
+        <span className="font-bold">Software Engineer</span> with{' '}
+        <span className="font-bold">3 years</span> of experience. I enjoy
+        building <span className="italic">sites</span>. My focus is{' '}
+        <span className="underline">React (NextJS)</span>.
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+        className="flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium"
+      >
+        <Link
+          href="contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer"
+        >
+          Contact me here{' '}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3  text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{' '}
+          <HiDownload className="group-hover:translate-x-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/huytm2302"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/Jayden-Truong"
+          target="_blank"
+        >
+          <BsGithub />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
