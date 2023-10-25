627651e08c4f17f77da72dc8c8d52f5f30179be1
Minh Huy <tmh.pug@gmail.com>
2023-10-25 11:13:50 +0700

    Create section heading & rename divider

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,17 @@
+'use client';
 import React from 'react';
+import SectionHeading from './SectionHeading';
+import { motion } from 'framer-motion';
 
 export default function About() {
   return (
-    <section className="mb-28 max-w-[45rem] leading-8 sm:mb-40 text-center">
-      <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+      className="mb-28 max-w-[45rem] leading-8 sm:mb-40 text-center"
+    >
+      <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{' '}
         <span className="font-medium">Accounting</span>, I decided to pursue my
@@ -30,6 +38,6 @@ export default function About() {
         <span className="font-medium">history and philosophy</span>. I'm also
         learning how to play the guitar.
       </p>
-    </section>
+    </motion.section>
   );
 }
--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-export default function Divider() {
+export default function SectionDivider() {
   return (
     <motion.div
       initial={{ opacity: 0, y: 100 }}
